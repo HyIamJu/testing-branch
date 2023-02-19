0bdc12b2b04570048010659c2ea331a107209614
HyIamJu <junandaikarizky10@gmail.com>
2023-02-19 10:17:20 +0700

    rencana buat development

--- helloworld.dart
+++ helloworld.dart
@@ -2,6 +2,8 @@ void main() {
   print("ini dari branch master");
 
   print("ini dari branch developments");
+  print("kedepannya akan buat fitur baru");
+
   print("tambah feature A");
   print("menambahkan feature b");
 }
