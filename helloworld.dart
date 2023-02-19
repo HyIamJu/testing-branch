01693ea5914ea8f9b67595ef2754f3e83522bd00
HyIamJu <junandaikarizky10@gmail.com>
2023-02-19 11:27:54 +0700

    tambah rencana kedepan feature b

--- helloworld.dart
+++ helloworld.dart
@@ -2,4 +2,5 @@ void main() {
   print("ini dari branch master");
   print("ini dari branch development");
   print("menambahkan feature b");
+  print("feature b nantinya juga akan ditambagkan agar kompleks");
 }
