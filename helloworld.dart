fd577e6da003e2183f3ce33f7d6f83cc6609daa6
HyIamJu <junandaikarizky10@gmail.com>
2024-05-04 12:15:29 +0700

    adding 5k loop test

--- helloworld.dart
+++ helloworld.dart
@@ -4,4 +4,10 @@ void main() {
   print("ini dari branch developments");
   print("tambah feature A");
   print("menambahkan feature b");
+
+  int i =0;
+  while (i < 5000) {
+    i++;
+    print("loop ke $i");
+  }
 }
